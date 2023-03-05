ed761ec9525d9bbfd737301964577d5bd6f52f90
xpwu <diodexp@foxmail.com>
2023-03-06 00:21:22 +0800

    master table

--- ktdbtable/src/main/java/com/github/xpwu/ktdbtable/db.kt
+++ ktdbtable/src/main/java/com/github/xpwu/ktdbtable/db.kt
@@ -2,7 +2,19 @@ package com.github.xpwu.ktdbtable
 
 import androidx.sqlite.db.SupportSQLiteDatabase
 
+
 interface DB : SupportSQLiteDatabase {
-  fun Exist(name: String):Boolean
+  interface Delegate {
+    fun onProgress(current: Int, total: Int)
+  }
+
+  fun Exist(table: String): Boolean
+  fun Version(table: String): Int
+  fun SetVersion(table: String): Int
+}
+
+
+fun onOpen(db: DB) {
+
 }
 
