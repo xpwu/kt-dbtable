6eeaef8cba0462a44d9007125b432f3f83e1c538
xpwu <diodexp@foxmail.com>
2023-03-09 16:21:20 +0800

    bak

--- ktdbtable-processor/src/main/java/com/github/xpwu/ktdbtable_processor/Processor.kt
+++ ktdbtable-processor/src/main/java/com/github/xpwu/ktdbtable_processor/Processor.kt
@@ -5,13 +5,23 @@ import com.github.xpwu.ktdbtble.annotation.Index
 import com.github.xpwu.ktdbtble.annotation.Table
 import com.google.auto.service.AutoService
 import javax.annotation.processing.AbstractProcessor
+import javax.annotation.processing.Messager
 import javax.annotation.processing.ProcessingEnvironment
 import javax.annotation.processing.RoundEnvironment
+import javax.lang.model.element.AnnotationMirror
+import javax.lang.model.element.AnnotationValue
+import javax.lang.model.element.Element
 import javax.lang.model.element.TypeElement
+import javax.tools.Diagnostic
 
 @AutoService(Processor::class)
 class Processor : AbstractProcessor() {
-  private var logger: Logger? = null
+  private var logger: Logger = Logger(object :Messager{
+    override fun printMessage(p0: Diagnostic.Kind?, p1: CharSequence?) {}
+    override fun printMessage(p0: Diagnostic.Kind?, p1: CharSequence?, p2: Element?) {}
+    override fun printMessage(p0: Diagnostic.Kind?, p1: CharSequence?, p2: Element?, p3: AnnotationMirror?) {}
+    override fun printMessage(p0: Diagnostic.Kind?, p1: CharSequence?, p2: Element?, p3: AnnotationMirror?, p4: AnnotationValue?) {}
+  })
 
   override fun init(processingEnv: ProcessingEnvironment) {
     super.init(processingEnv)
