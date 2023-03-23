7214800cbed9ae4add28a219040b91d2e91ea1f5
xpwu <diodexp@foxmail.com>
2023-03-24 03:48:28 +0800

    Update annotation.kt

--- ktdbtable-annotation/src/main/java/com/github/xpwu/ktdbtble/annotation/annotation.kt
+++ ktdbtable-annotation/src/main/java/com/github/xpwu/ktdbtble/annotation/annotation.kt
@@ -30,7 +30,7 @@ The new column may take any of the forms permissible in a CREATE TABLE statement
  * Boolean => INTEGER ( true -> 1 ; false -> 0 )
  * String => TEXT
  * Float, Double => REAL
- * ByteArray, Array<Byte> => BLOB
+ * ByteArray => BLOB
  * 其他类型会报错
  *
  * todo 支持自定义的转换函数
@@ -39,16 +39,18 @@ The new column may take any of the forms permissible in a CREATE TABLE statement
  *
  * CHECK 约束：CHECK 约束确保某列中的所有值满足一定条件, 暂未支持 // todo
  *
- * @param name: 如果为"_"，则忽略此字段
+ * @param name: 表的列名
  *
  *   https://www.sqlite.org/lang_createtable.html
- * @param primaryKey PRIMARY Key 约束，同时也必须是NOT NULL，字段不能是 option 的。MULTI_约束的顺序与sequence定义的顺序一致
+ * @param primaryKey PRIMARY KEY 约束，字段不能是 option 的(PRIMARY KEY 默认是 NOT NULL)。
+ *  MULTI_约束的顺序与sequence定义的顺序一致
  *
- * @param notNull NOT NULL, 如果属性是 option 的定义，notNull必须为false，即使设定notNull为true，该设置也不生效。
+ * @param notNull 谨慎使用此属性，新增列时，会造成O(n)的时间复杂度。
+ *  NOT NULL, 如果属性是 option 的定义，notNull必须为false，即使设定notNull为true，该设置也不生效。
  *
  * @param defaultValue 以字符串的方式设置字段的默认值，设置的值在创建表时直接使用于 DEFAULT xxx 中
  *
- * @param sequence PRIMARY KEY 约束的顺序，只要相对有大小比较就行，并不严格要求数字是连续的
+ * @param sequence PRIMARY KEY 联合约束的顺序，只要相对有大小比较就行，并不严格要求数字是连续的
  *
  */
 @Target(AnnotationTarget.FIELD)
